9cc9584ab4beeafb1d37d8d2efc07abaf8a61586
Shreyas Shankar <shreyas.shankar920@gmail.com>
2019-10-30 16:42:18 +0530

    round robin added

--- servers/views.py
+++ servers/views.py
@@ -53,6 +53,22 @@ class LeastConnections(CreateView):
     fields = ['type']
     template_name = 'servers/createProcess.html'
 
+    def render_to_response(self, context, **response_kwargs):
+        """
+        Return a response, using the `response_class` for this view, with a
+        template rendered with the given context.
+        Pass response_kwargs to the constructor of the response class.
+        """
+        response_kwargs.setdefault('content_type', self.content_type)
+        context['title']="Least Connections"
+        return self.response_class(
+            request=self.request,
+            template=self.get_template_names(),
+            context=context,
+            using=self.template_engine,
+            **response_kwargs
+        )
+
 
     def form_valid(self, form):
         """If the form is valid, save the associated model."""
@@ -129,7 +145,21 @@ class RoundRobin(CreateView):
     fields = ['type']
     template_name = 'servers/createProcess.html'
 
-
+    def render_to_response(self, context, **response_kwargs):
+        """
+        Return a response, using the `response_class` for this view, with a
+        template rendered with the given context.
+        Pass response_kwargs to the constructor of the response class.
+        """
+        response_kwargs.setdefault('content_type', self.content_type)
+        context['title']="Round Robin"
+        return self.response_class(
+            request=self.request,
+            template=self.get_template_names(),
+            context=context,
+            using=self.template_engine,
+            **response_kwargs
+        )
 
 
     def form_valid(self, form):
